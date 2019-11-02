4ea17203bdadf7aa154294dd0ba585ef320a11a6
jmstfv <github@jmstfv.com>
2019-11-02 21:54:29 +0400

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Game.create!(
+  title: 'Napoli - Bologna',
+  league: 'Serie A',
+  home_team_logo: 'https://d1icd6shlvmxi6.cloudfront.net/gsc/1J0N7O/63/4e/ea/634eeaeb893248d1a4de81afa3d03e55/images/wireframes/u24.png?token=8bdbd3569363300ad7939f3be3e9e91ccd13bb2565db7784555975db4a08e2b5',
+  away_team_logo: 'https://d1icd6shlvmxi6.cloudfront.net/gsc/1J0N7O/63/4e/ea/634eeaeb893248d1a4de81afa3d03e55/images/wireframes/u27.png?token=c76354d088b42b0bc33c6d569585416130a1d4f53f0325c5a276dfb35d66dc00',
+  game_time: Time.zone.local(2018, 2, 11, 14, 30),
+  channel: 'Rai1',
+  reach: '4_000_000',
+  price_per_minute: 3200,
+  minutes_booked: 0,
+)
+
+Game.create!(
+  title: 'Udinese - Spal',
+  home_team_logo: 'https://d1icd6shlvmxi6.cloudfront.net/gsc/1J0N7O/63/4e/ea/634eeaeb893248d1a4de81afa3d03e55/images/wireframes/u25.png?token=509214caa34309787f7a47cbe8dec6a65a2e4e0e49cf77ecf6adce6ef0c0e167',
+  away_team_logo: 'https://d1icd6shlvmxi6.cloudfront.net/gsc/1J0N7O/63/4e/ea/634eeaeb893248d1a4de81afa3d03e55/images/wireframes/u28.png?token=4d8fee8d07105dad0223e8ae23cc2066b782482937fde8b353f6d1bb7c356d3c',
+  game_time: Time.zone.local(2018, 2, 11, 14, 30),
+  league: 'Serie A',
+  channel: 'Rai1',
+  reach: '3_000_000',
+  price_per_minute: 3600,
+  minutes_booked: 0,
+)
+
+Game.create!(
+  title: 'Juventus - Genoa',
+  home_team_logo: 'https://d1icd6shlvmxi6.cloudfront.net/gsc/1J0N7O/63/4e/ea/634eeaeb893248d1a4de81afa3d03e55/images/wireframes/u26.jpg?token=26849654000f0c7fac682374bc8384c35c3cc01087a7946dd93e30ad78bbcaf9',
+  away_team_logo: 'https://d1icd6shlvmxi6.cloudfront.net/gsc/1J0N7O/63/4e/ea/634eeaeb893248d1a4de81afa3d03e55/images/wireframes/u29.png?token=ce9fae01642810cce5731e26343f573621becd27ef5ad7c0a7400aaa25bac552',
+  game_time: Time.zone.local(2018, 2, 11, 14, 30),
+  league: 'Serie A',
+  channel: 'Rai1',
+  reach: '6_000_000',
+  price_per_minute: 4500,
+  minutes_booked: 0,
+)
