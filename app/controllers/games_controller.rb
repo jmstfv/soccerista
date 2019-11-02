d62919d58f06d3aa25de980974d1a8ca91da95d9
jmstfv <github@jmstfv.com>
2019-11-02 21:39:32 +0400

    add scaffolding for games controller

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,29 @@
+class GamesController < ApplicationController
+  before_action :set_game, only: [:update]
+
+  def index
+    @games = Game.all
+  end
+
+  def update
+    # there are no checks whether any game is overbooked or not
+    # there should be one check on the database level,
+    # and another check on the ActiveRecond level (e.g. minutes_booked <= 90)
+    @game.update(games_params)
+    @game.save
+
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  private
+
+    def set_game
+      @game = Game.find(params[:id])
+    end
+
+    def games_params
+      params.require(:game).permit(:minutes_booked)
+    end
+end
